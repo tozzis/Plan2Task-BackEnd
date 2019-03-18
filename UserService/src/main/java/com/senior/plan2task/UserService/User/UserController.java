2570dfbca734a05b2f9be82b312a4f2e03d17c78
top <patcharapol.top@mail.kmutt.ac.th>
2019-03-18 14:53:24 +0700

    [EDIT] search user by email

--- UserService/src/main/java/com/senior/plan2task/UserService/User/UserController.java
+++ UserService/src/main/java/com/senior/plan2task/UserService/User/UserController.java
@@ -5,12 +5,15 @@ import com.senior.plan2task.UserService.Facebook.FacebookAccount;
 import com.senior.plan2task.UserService.Filter.TokenAuthenticationService;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -68,5 +71,11 @@ public class UserController {
                 }
             }
         }
+    }
+    
+    @PostMapping("/user/email")
+    public ResponseEntity<User> getUserByEmail(@RequestBody Map<String, String> userEmail) {
+        User user = userService.getUserByEmail(userEmail.get("email"));
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
