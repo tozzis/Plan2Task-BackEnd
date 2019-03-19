612bb9d4f2404e89119eeafa460ac1f76f9b9294
top <patcharapol.top@mail.kmutt.ac.th>
2019-03-19 21:01:12 +0700

    [EDIT] ทำ env ของ userService

--- PlanService/src/main/java/com/senior/plan2task/PlanService/Filter/GlobalValue.java
+++ PlanService/src/main/java/com/senior/plan2task/PlanService/Filter/GlobalValue.java
@@ -7,9 +7,17 @@ import org.springframework.stereotype.Component;
 public class GlobalValue {
     
     public static String secretKey;
+    
+    public static String userService;
 
     @Value("${authenservice.jwt.secret}")
     public void setSecretKey(String secretKey) {
         GlobalValue.secretKey = secretKey;
     }
+
+    @Value("${authenservice.url.userService}")
+    public void setUserService(String userService) {
+        GlobalValue.userService = userService;
+    }
+    
 }
