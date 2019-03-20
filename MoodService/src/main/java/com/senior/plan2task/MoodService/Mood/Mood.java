68377e31ab0fde3d443acac0c78e711cc36f45ff
pasutnon <pasut.non@mail.kmutt.ac.th>
2019-03-20 21:41:00 +0700

    [ADD] Mood Model

--- MoodService/src/main/java/com/senior/plan2task/MoodService/Mood/Mood.java
+++ MoodService/src/main/java/com/senior/plan2task/MoodService/Mood/Mood.java
@@ -1,5 +1,51 @@
 package com.senior.plan2task.MoodService.Mood;
 
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "moods")
 public class Mood {
-    
+
+    @Id
+    private String id;
+
+    private String moodName;
+
+    private Date moodDate;
+
+    public Mood() {
+    }
+
+    public Mood(String id, String moodName, Date moodDate) {
+        this.id = id;
+        this.moodName = moodName;
+        this.moodDate = moodDate;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMoodName() {
+        return this.moodName;
+    }
+
+    public void setMoodName(String moodName) {
+        this.moodName = moodName;
+    }
+
+    public Date getMoodDate() {
+        return this.moodDate;
+    }
+
+    public void setMoodDate(Date moodDate) {
+        this.moodDate = moodDate;
+    }
+
 }
