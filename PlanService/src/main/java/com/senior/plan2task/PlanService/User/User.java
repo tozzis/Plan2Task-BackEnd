11dc6b8e5e2c00d1b835f9a3ed759fb16c04ec61
pasutnon <pasut.non@mail.kmutt.ac.th>
2019-03-20 22:18:44 +0700

    [UPDATE] User In UserAdapter

--- PlanService/src/main/java/com/senior/plan2task/PlanService/User/User.java
+++ PlanService/src/main/java/com/senior/plan2task/PlanService/User/User.java
@@ -5,7 +5,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "users")
 public class User {
     
     @Id
