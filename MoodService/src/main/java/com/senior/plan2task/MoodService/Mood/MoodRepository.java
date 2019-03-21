0aee72c63710e9dc8467031050c5982bb75ed987
pasutnon <pasut.non@mail.kmutt.ac.th>
2019-03-21 11:35:19 +0700

    [UPDATE] ADD User to Mood

--- MoodService/src/main/java/com/senior/plan2task/MoodService/Mood/MoodRepository.java
+++ MoodService/src/main/java/com/senior/plan2task/MoodService/Mood/MoodRepository.java
@@ -1,7 +1,12 @@
 package com.senior.plan2task.MoodService.Mood;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MoodRepository extends MongoRepository<Mood, String> {
+
+    public List<Mood> findByUserId(String userId);
+
 }
 
