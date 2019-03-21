fcc6f004fbb23727b0db6b95de9627a653d85fa4
pasutnon <pasut.non@mail.kmutt.ac.th>
2019-03-21 11:36:57 +0700

    [UPDATE] relate Moodtype to Mood

--- MoodService/src/main/java/com/senior/plan2task/MoodService/MoodType/MoodTypeRepository.java
+++ MoodService/src/main/java/com/senior/plan2task/MoodService/MoodType/MoodTypeRepository.java
@@ -3,6 +3,6 @@ package com.senior.plan2task.MoodService.MoodType;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface MoodTypeRepository extends MongoRepository<MoodType, String> {
-    MoodType findMoodTypeImageByMoodId(String mood_id);
+    // MoodType findMoodTypeImageByMoodId(String moodId);
 }
 
