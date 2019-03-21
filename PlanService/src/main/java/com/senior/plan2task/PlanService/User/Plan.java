e5b209e3020deef60aa68939cdf0b9849b483c38
gus43980 <nattapon.roeksriprapa@mail.kmutt.ac.th>
2019-03-21 10:03:24 +0700

    [ADD] Plan

---
+++ PlanService/src/main/java/com/senior/plan2task/PlanService/User/Plan.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.senior.plan2task.PlanService.User;
+import java.util.Date;
+import javax.validation.constraints.Email;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+/**
+ *
+ * @author New_pc
+ */
+@Document(collection = "plans")
+public class Plan {
+ @Id
+    private String id;
+    private String title;
+    private String detail;
+    private Date startDate;
+    private Date endDate;
+    private String location;
+    private User member;
+    private String type;
+    private boolean status;
+    private String notification;
+    private String userId;
+
+    public Plan(String userId) {
+        this.userId = userId;
+    }
+
+    public Plan(String id, String title, String detail, Date startDate, Date endDate, String location, User member, String type, boolean status, String notification, String userId) {
+        this.id = id;
+        this.title = title;
+        this.detail = detail;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.member = member;
+        this.type = type;
+        this.status = status;
+        this.notification = notification;
+        this.userId = userId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public User getMember() {
+        return member;
+    }
+
+    public void setMember(User member) {
+        this.member = member;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getNotification() {
+        return notification;
+    }
+
+    public void setNotification(String notification) {
+        this.notification = notification;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    
+    
+    
+    
+
+}
