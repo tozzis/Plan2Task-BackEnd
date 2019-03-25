9456032c0d0bb001f69573091d8df38a92337e9a
top <patcharapol.top@mail.kmutt.ac.th>
2019-03-25 13:18:38 +0700

    [ADD] create UserRequestEmail

---
+++ FriendService/src/main/java/com/senior/plan2task/FriendService/User/UserRequestEmail.java
@@ -0,0 +1,22 @@
+package com.senior.plan2task.FriendService.User;
+
+public class UserRequestEmail {
+    
+    private String email;
+
+    public UserRequestEmail() {
+    }
+
+    public UserRequestEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
