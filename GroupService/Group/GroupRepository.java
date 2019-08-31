e145514f6cb3a607799944b29dd8d41123b25312
pasutnon <pasut.non@mail.kmutt.ac.th>
2019-08-31 12:29:52 +0700

    no message

--- GroupService/Group/GroupRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senior.plan2task.GroupService.Group;
-
-import java.util.List;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface GroupRepository extends MongoRepository<Group, String> {
-
-}
